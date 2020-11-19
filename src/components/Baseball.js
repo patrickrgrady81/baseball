c135fe13ae748a8e64a650febb2d76ecca957828
Patrick Grady <patickrgrady81@gmail.com>
2020-11-19 12:15:47 -0500

    start

---
+++ src/components/Baseball.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Baseball extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Inside Baseball App</h1>
+      </div>
+    )
+  }
+}
+
+export default Baseball
