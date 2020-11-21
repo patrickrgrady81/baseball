1ef97692965b3effff016a1f1639801efbd36e68
Patrick Grady <patickrgrady81@gmail.com>
2020-11-21 12:15:43 -0500

    Added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import Baseball from "./components/baseball/Baseball"
+import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+
 import Nav from "./components/nav/Nav"
+import Home from "./components/pages/Home"
+import Baseball from "./components/baseball/Baseball"
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Baseball />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/baseball" component={Baseball} />
+      </div>
+    </Router>
+
+
   );
 }
 
